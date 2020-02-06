8bfd1e2f2d7b9179f61bcd1f5b6e6f4fb9d15c68
sregati <57911021+sregati@users.noreply.github.com>
2020-02-06 12:54:48 -0600

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { MovieCardComponent } from './components/movie-card/movie-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MoviesComponent,
+    MovieListComponent,
+    MovieCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
