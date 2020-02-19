19428cc3afaaf10cd618ed94815cf2dd5e511914
Sagar Limaye <sagarl3232@hotmail.com>
2020-02-19 15:35:45 -0600

    fixed url path for movie crud service

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -10,16 +10,16 @@ export class MoviesService {
   constructor(private http: HttpClient) { }
 
   getMovies() {
-    return this.http.get<Movie[]>('/mrs/movies-ws/');
+    return this.http.get<Movie[]>('/mrs/movies-ws/movies/');
   }
   getMovieById(id: number) {
-    return this.http.get('/mrs/movies-ws/'+id);
+    return this.http.get('/mrs/movies-ws/movie'+id);
   }
   updateMovie(movie: Movie) {
-    return this.http.put('/mrs/movies-ws/', movie);
+    return this.http.put('/mrs/movies-ws/movie', movie);
   }
   removeMovieById(id: number) {
-    return this.http.delete('/mrs/movies-ws/'+id);
+    return this.http.delete('/mrs/movies-ws/movie'+id);
   }
   
 }
